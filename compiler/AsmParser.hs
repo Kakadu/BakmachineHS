b22d811e3b3e8aec6c7ff621e5e525c79a5410b4
kakadu <kakadu.hafanana@gmail.com>
2012-08-14 22:33:35 +0400

    added cmp operator

--- compiler/AsmParser.hs
+++ compiler/AsmParser.hs
@@ -3,7 +3,7 @@ module AsmParser (
   inter, noop, program
 ) where
 
-
+import Debug.Trace
 import Text.Parsec
 import Text.Parsec.String
 import Types
@@ -11,7 +11,7 @@ import Data.Word
 
 register :: Stream s m Char => ParsecT s () m RegisterSort
 register =
-  (h "ah" AH) <|> (h "bh" BH) <|> (h "bh" BH) <|> (h "bh" BH) <|> (h "bh" BH) 
+  (h "ah" AH) <|> (h "bh" BH) <|> (h "ch" CH) <|> (h "dh" DH) <|> (h "eh" DH) 
   where h s r = string s >> return r
 
 inter :: Stream s m Char => ParsecT s () m Bytecmd
@@ -19,10 +19,11 @@ inter =
   do
     string "int"
     spaces
-    digits <- many1 digit
-    let i = read digits :: Word8
-    case  Types.inter_of_int i of
-      Just x  -> return $ Interrupt x
+    digits <- (many1 digit) <?> "need some digits"
+    let i = trace (show digits) (read digits :: Word8)
+    case Types.inter_of_int i of
+      Just x  -> return (trace (show ans) ans )
+                 where ans = Interrupt x
       Nothing -> fail "no such interrupt"
       
 int :: Stream s m Char => ParsecT s () m Word8
@@ -35,7 +36,7 @@ noop = string "nop" >> (return Nop)
 pair :: Stream s m Char => ParsecT s () m a -> ParsecT s () m b -> (a -> b -> c) -> ParsecT s () m c
 pair p1 p2 f =
   do l <- p1
-     char ','
+     spaces
      r <- p2
      return $ (f l r)
      
@@ -46,7 +47,14 @@ mov =
     spaces
     ans <- (pair int register Mov1) <|> (pair register register Mov2)
     return ans 
-         
+
+cmp :: Stream s m Char => ParsecT s () m Bytecmd     
+cmp = 
+  do 
+    string "cmp"
+    spaces
+    ans <- (pair register register Cmp2)
+    return ans
 
 program :: Stream s m Char => ParsecT s () m [Bytecmd]
 program = 
@@ -55,7 +63,7 @@ program =
      return ans
   where cmd =             
           do 
-            x <- inter <|> noop <|> mov
+            x <- inter <|> noop <|> mov <|> cmp
             --spaces
             char '\n' 
             return x
--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,7 @@
 module Types (
   RegisterSort (AH,BH,CH,DH,EH),
-  InterruptSort (IExit, IOutInt),
-  Bytecmd (Nop, Interrupt, Mov1, Mov2),
+  InterruptSort (IExit, IInputInt, IOutInt),
+  Bytecmd (Nop, Interrupt, Mov1, Mov2, Cmp2),
   inter_of_int,
   code_of_reg,
   code_of_inter
@@ -14,12 +14,14 @@ data RegisterSort = AH | BH | CH | DH | EH | SP deriving (Show)
 
 data InterruptSort = 
     IExit
+  | IInputInt
   | IOutInt deriving (Show)
 
 data Bytecmd =
     Mov1 Word8 RegisterSort 
   | Mov2 RegisterSort RegisterSort
   | Nop
+  | Cmp2 RegisterSort RegisterSort
   | Interrupt InterruptSort
   deriving (Show)
 
@@ -34,8 +36,10 @@ code_of_reg x = case x of
 
 inter_of_int 10 = Just IExit
 inter_of_int 11 = Just IOutInt
+inter_of_int 13 = Just IInputInt
 inter_of_int __ = Nothing
   
 code_of_inter :: InterruptSort -> Word8
 code_of_inter IExit = 10
-code_of_inter IOutInt = 13
+code_of_inter IOutInt = 11
+code_of_inter IInputInt = 13
